ca9e81c12358e45701ad3a63043f5f850c3f3270
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-08-31 11:29:30 +0300

    Add files via upload

---
+++ lesson3_task01.erl
@@ -0,0 +1,13 @@
+-module(lesson3_task01).
+-export([first_word/1]).
+
+first_word(Bin) ->
+  first_word(Bin, <<>>).
+
+
+first_word(<<$\s, _/binary>>, Acc) ->
+  Acc;
+first_word(<<X, Bin/binary>>, Acc) ->
+  first_word(Bin, <<Acc/binary, X>>);
+first_word(<<>>,Acc) ->
+  Acc.
---
+++ lesson3_task02.erl
@@ -0,0 +1,11 @@
+-module(lesson3_task02).
+-export([words/1]).
+
+words(Bin) -> words(Bin,<<>>,[]).
+words(<<>>,Acc, List) -> reverse([Acc|List]);
+words(<<$\s, Rest/binary>>, Acc,List) -> words(Rest, <<>> ,[Acc|List]);
+words(<<X, Rest/binary>>,Acc,List) -> words(Rest, <<Acc/binary, X>>,List).
+
+reverse(L) -> reverse(L,[]).
+reverse([],Acc) -> Acc;
+reverse([H|T],Acc) -> reverse(T,[H|Acc]).
