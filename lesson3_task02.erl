cf849c642b142659ef2cd4046a9447f8d6164f3c
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-02 22:45:16 +0300

    Update lesson3_task02.erl

--- lesson3_task02.erl
+++ lesson3_task02.erl
@@ -1,11 +1,18 @@
 -module(lesson3_task02).
 -export([words/1]).
 
-words(Bin) -> words(Bin,<<>>,[]).
-words(<<>>,Acc, List) -> reverse([Acc|List]);
-words(<<$\s, Rest/binary>>, Acc,List) -> words(Rest, <<>> ,[Acc|List]);
-words(<<X, Rest/binary>>,Acc,List) -> words(Rest, <<Acc/binary, X>>,List).
+words(Bin) ->
+    words(Bin,<<>>,[]).
+words(<<>>,Acc, List) ->
+    reverse([Acc|List]);
+words(<<$\s, Rest/binary>>, Acc,List) ->
+    words(Rest, <<>> ,[Acc|List]);
+words(<<X, Rest/binary>>,Acc,List) -> 
+    words(Rest, <<Acc/binary, X>>,List).
 
-reverse(L) -> reverse(L,[]).
-reverse([],Acc) -> Acc;
-reverse([H|T],Acc) -> reverse(T,[H|Acc]).
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Acc) ->
+    Acc;
+reverse([H|T],Acc) ->
+    reverse(T,[H|Acc]).
