e0bd21207341ca404991dd2197e5d5068df2db4b
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-02 22:46:13 +0300

    Update lesson3_task03.erl

--- lesson3_task03.erl
+++ lesson3_task03.erl
@@ -1,16 +1,25 @@
 -module(lesson3_task03).
 -export([split/2]).
 
-split(Bin,Delimiter) -> split(Bin,list_to_binary(Delimiter),<<>>,[]).
-split(<<>>,_,Acc,List) -> reverse([<<Acc/binary>>|List]);
+split(Bin,Delimiter) -> 
+    split(Bin,list_to_binary(Delimiter),<<>>,[]).
+split(<<>>,_,Acc,List) ->
+    reverse([<<Acc/binary>>|List]);
 
-split(Bin,Delimiter,Acc,List) -> DelSize = (byte_size(Delimiter)),
+split(Bin,Delimiter,Acc,List) ->
+    DelSize = (byte_size(Delimiter)),
   case Bin of
-    <<Delimiter:DelSize/binary, Rest/binary>> -> split(Rest,Delimiter,<<>>,[<<Acc/binary>>|List]);
-    <<X, Rest/binary>> -> split(Rest,Delimiter,<<Acc/binary, X>>,List);
-    _ -> split(Bin,Delimiter,Acc,List)
+    <<Delimiter:DelSize/binary, Rest/binary>> -> 
+         split(Rest,Delimiter,<<>>,[<<Acc/binary>>|List]);
+    <<X, Rest/binary>> -> 
+         split(Rest,Delimiter,<<Acc/binary, X>>,List);
+    _ -> 
+         split(Bin,Delimiter,Acc,List)
   end.
 
-reverse(L) -> reverse(L,[]).
-reverse([],Acc) -> Acc;
-reverse([H|T],Acc) -> reverse(T,[H|Acc]).
+reverse(L) ->
+     reverse(L,[]).
+reverse([],Acc) -> 
+     Acc;
+reverse([H|T],Acc) ->
+    reverse(T,[H|Acc]).
