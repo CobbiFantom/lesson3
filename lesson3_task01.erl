ce6d4c67e0f8214c3c8db7c5f15c19ccd1c25f0b
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-05 13:13:32 +0300

    Update lesson3_task01.erl

--- lesson3_task01.erl
+++ lesson3_task01.erl
@@ -1,13 +1,14 @@
 -module(lesson3_task01).
+
 -export([first_word/1]).
 
 first_word(Bin) ->
-  first_word(Bin, <<>>).
+    first_word(Bin, <<>>).
 
 
 first_word(<<$\s, _/binary>>, Acc) ->
-  Acc;
-first_word(<<X, Bin/binary>>, Acc) ->
-  first_word(Bin, <<Acc/binary, X>>);
+    Acc;
+first_word(<<X/utf8, Bin/binary>>, Acc) ->
+    first_word(Bin, <<Acc/binary, X>>);
 first_word(<<>>,Acc) ->
-  Acc.
+    Acc.
