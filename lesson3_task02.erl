8ca4578c1f4f4cc4f725bdd752c0f75574cf4151
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-06 15:09:33 +0300

    Update lesson3_task02.erl

--- lesson3_task02.erl
+++ lesson3_task02.erl
@@ -8,7 +8,7 @@ words(<<>>,Acc, List) ->
     reverse([Acc|List]);
 words(<<$\s, Rest/binary>>, Acc,List) ->
     words(Rest, <<>> ,[Acc|List]);
-words(<<X, Rest/binary>>,Acc,List) -> 
+words(<<X/utf8, Rest/binary>>,Acc,List) -> 
     words(Rest, <<Acc/binary, X>>,List).
 
 reverse(L) ->
