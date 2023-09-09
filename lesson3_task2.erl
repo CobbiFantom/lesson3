abc38dfd17fd18e77617edbeba3ab2b0484682a2
Arthur Sivkov <80418177+CobbiFantom@users.noreply.github.com>
2023-09-09 14:25:07 +0300

    Update

--- lesson3_task2.erl
+++ lesson3_task2.erl
@@ -1,15 +1,16 @@
--module(lesson3_task02).
+-module(lesson3_task2).
 
 -export([words/1]).
 
 words(Bin) ->
     words(Bin,<<>>,[]).
-words(<<>>,Acc, List) ->
-    reverse([Acc|List]);
+
 words(<<$\s, Rest/binary>>, Acc,List) ->
     words(Rest, <<>> ,[Acc|List]);
-words(<<X/utf8, Rest/binary>>,Acc,List) -> 
-    words(Rest, <<Acc/binary, X>>,List).
+words(<<X/utf8, Rest/binary>>,Acc,List) ->
+    words(Rest, <<Acc/binary, X/utf8>>, List);
+words(<<>>,Acc, List) ->
+    reverse([Acc|List]).
 
 reverse(L) ->
     reverse(L,[]).
